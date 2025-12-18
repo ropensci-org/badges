4eeac5b5af7c46f963b852aa64105cfb2b857971
mpadge <mark.padgham@email.com>
2025-12-18 12:13:50 +0100

    update 'check-registry' to use r-univ api rather than rosregistry; closes #26

--- check-registry.R
+++ check-registry.R
@@ -9,65 +9,26 @@ if (!file.exists (ob)) {
     stop ("onboarded.json not found")
 }
 json_data <- jsonlite::read_json (ob, simplifyVector = TRUE)
-json_data <- json_data [json_data$status == "reviewed", ]
+json_data <- json_data [json_data$status == "reviewed", ] |>
+    dplyr::rename (package_onboarded = pkgname)
 
-u_base <- "https://api.github.com/repos/ropensci/roregistry/"
-u <- paste0 (u_base, "contents/packages.json")
-ftmp <- tempfile (fileext = ".json")
-pj <- gh::gh (
-    u,
-    .accept = "application/vnd.github.raw+json",
-    .destfile = ftmp
-)
-pj <- jsonlite::read_json (ftmp, simplifyVector = TRUE)
-file.remove (ftmp)
+u <- "https://ropensci.r-universe.dev/api/packages/"
+pj <- jsonlite::read_json (u, simplifyVector = TRUE)
+meta <- pj$`_metadata`
+index <- which (!is.na (meta$review$id))
+pj_dat <- data.frame (
+    package_current = pj$Package,
+    url = pj$`_devurl`,
+    iss_no = meta$review$id,
+    status = meta$review$status,
+    review_url = meta$review$url
+) [index, ]
+pj_dat <- dplyr::left_join (pj_dat, json_data, by = "iss_no")
 
-# Check against codemeta created in 'makeregistry', which extracts actual
-# current package names:
-cm_url <- "https://github.com/ropensci/roregistry/blob/gh-pages/raw_cm.json?raw=true"
-cm <- jsonlite::read_json (cm_url)
-cm <- cm[lengths(cm) > 0]
-keep <- c ("identifier", "name", "codeRepository")
-cm <- lapply (cm, function (i) {
-    revdat <- ifelse (length (i$review) > 0L, i$review$url, NA_character_)
-    data.frame (c (i [keep], review = revdat))
-})
-cm <- do.call (rbind, cm)
-
-cm_pj <- dplyr::left_join (pj, cm, by = c ("url" = "codeRepository"))
-
-index <- which (!is.na (cm_pj$review) & is.na (cm_pj$metadata$review$id))
-# packages.json has metadata consisting of:
-# - review$id
-# - review$status
-# - review$version
-# - review$organization
-# - review$url
-
-ids <- regmatches (
-    cm_pj$review [index],
-    regexpr ("\\/[0-9]+$", cm_pj$review [index])
-)
-ids <- as.integer (gsub ("^\\/", "", ids))
-cm_pj$metadata$review$id [index] <- ids
-cm_pj$metadata$review$url [index] <- cm_pj$review [index]
-
-# The `json_data` include packages which have been archived on GitHub.
-# These no longer appear at all in packages.json, so reduce `pj` data down to
-# only those in the registry:
-pj_reg <- cm_pj [which (!is.na (cm_pj$metadata$review$id)), ]
-pj_reg <- data.frame (
-    package_pj = pj_reg$package,
-    status = pj_reg$metadata$review$status,
-    iss_no = pj_reg$metadata$review$id
-)
-pj_reg <- pj_reg [which (pj_reg$iss_no %in% json_data$iss_no), ]
-index <- match (pj_reg$iss_no, json_data$iss_no)
-pj_reg$pkgname <- json_data$pkgname [index]
-
-# At that point, "package_pj" is the actual package DESC name taken from
-# packages.json, and "pkgname" is from "onboarded.json" here, written here in
-# the "update_badges.R" script, and take from GitHub issue titles.
+# At that point, "pj_dat" has "package_current" as the actual package DESC name
+# taken from packages.json, and "package_onboarded" as the name taken from
+# "onboarded.json" which is written in the "update_badges.R" script, and
+# ultimately taken from the template at the top of the GitHub review issues.
 #
 # Mismatches may occur because either:
 # 1. One review yielded muliple packages (e.g., targets, babette), or
@@ -75,25 +36,25 @@ pj_reg$pkgname <- json_data$pkgname [index]
 #
 # We need to ignore the first, and identify only the second casese.
 
-index <- which (pj_reg$pkgname != pj_reg$package_pj)
+index <- which (pj_dat$package != pj_dat$pkgname)
 if (length (index) > 0) {
-    mismatch_issues <- sort (unique (pj_reg$iss_no [index]))
-    pj_reg_mismatch <- pj_reg [which (pj_reg$iss_no %in% mismatch_issues), ]
-    # Truly mmimatches issues will then only have a single "iss_no" value in
+    mismatch_issues <- sort (unique (pj_dat$iss_no [index]))
+    pj_dat_mismatch <- pj_dat [which (pj_dat$iss_no %in% mismatch_issues), ]
+    # Truly mmimatched issues will then only have a single "iss_no" value in
     # that table:
-    mismatch_issues <- table (pj_reg_mismatch$iss_no)
+    mismatch_issues <- table (pj_dat_mismatch$iss_no)
     mismatch_issues <- as.integer (names (mismatch_issues) [which (mismatch_issues == 1L)])
-    index <- which (pj_reg$iss_no %in% mismatch_issues)
+    index <- which (pj_dat$iss_no %in% mismatch_issues)
 }
 
 if (length (index) > 0) {
 
-    pj_mismatch <- pj_reg [index, ]
+    pj_mismatch <- pj_dat [index, ]
     hrefs <- paste0 (
         " - [ ] https://github.com/ropensci/software-review/issues/",
         pj_mismatch$iss_no,
-        "  '", pj_mismatch$package_pj, "' in roregistry;  '",
-        pj_mismatch$pkgname, "' in review thread"
+        "  '", pj_mismatch$package_current, "' in roregistry;  '",
+        pj_mismatch$package_onboarded, "' in review thread"
     )
     iss_msg <- paste0 (
         "The following software review issue",
